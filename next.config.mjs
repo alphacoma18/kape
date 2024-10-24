9e8d04b234b9290b7e0af4a1a7762a618618c0bc
No-Research <75786889+No-Research@users.noreply.github.com>
2024-10-24 15:30:07 +0800

    config: updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+     basePath: "/2048-in-react",
      output: "export",
-     images: { unoptimized: true }
+     images: { unoptimized: true },
+     reactStrictMode: true
 };
 
 export default nextConfig;
