09cfb3c38ebce9c1e531220dd1c7590744b84867
Alpha Romer Coma <alpharomercoma@proton.me>
2024-10-28 01:18:32 +0800

    feat: add PWA config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,27 @@
 /** @type {import('next').NextConfig} */
-
-
-const isProd = process.env.NODE_ENV === "production";  
-
+import withPWA from "next-pwa";
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
      basePath: isProd ? "/kape" : "",
      output: "export",
      images: { unoptimized: true },
-     reactStrictMode: true
+     reactStrictMode: true,
+     swcMinify: true,            // Enable SWC minification for improved performance
+     compiler: {
+          removeConsole: !isProd,
+     }
 };
 
-export default nextConfig;
+export default withPWA({
+     dest: "public",         // destination directory for the PWA files
+     disable: !isProd,      // disable PWA in the development environment
+     register: true,         // register the PWA service worker
+     skipWaiting: true,      // skip waiting for service worker activation
+     cacheStartUrl: true,
+     skipWaiting: true,
+     scope: '/',
+     cacheId: 'kape',
+     cleanupOutdatedCaches: true,
+     clientsClaim: true,
+     navigationPreload: true,
+})(nextConfig);
